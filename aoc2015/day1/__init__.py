4406b75982900bc84c2575fa21f1bd1a7a649896
James K. Pringle <jameskpringle@gmail.com>
2021-03-12 11:46:01 -0600

    Solve Day 1, a warm up

---
+++ aoc2015/day1/__init__.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from typing import List
+
+
+def part1(lines: List[str]):
+    instructions = lines[0]
+    counter = Counter(instructions)
+    return counter.get('(') - counter.get(')')
+
+
+def part2(lines: List[str]):
+    location = 0
+    instructions = lines[0]
+    for i, item in enumerate(instructions, start=1):
+        if item == '(':
+            location += 1
+        else:
+            location -= 1
+        if location < 0:
+            return i
+    else:
+        raise ValueError("Never went negative!")
