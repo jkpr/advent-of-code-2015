9820f15e5a3faf01713c469e69eefcbb5862ebdf
James K. Pringle <jameskpringle@gmail.com>
2021-03-12 11:54:07 -0600

    Solve Day 2

---
+++ aoc2015/day2/__init__.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def part1(lines: List[str]):
+    total_sq_ft = 0
+    for line in lines:
+        a, b, c = sorted(int(i) for i in line.split("x"))
+        total_sq_ft += 3*a*b + 2*a*c + 2*b*c
+    return total_sq_ft
+
+
+def part2(lines: List[str]):
+    total_ft = 0
+    for line in lines:
+        a, b, c = sorted(int(i) for i in line.split("x"))
+        total_ft += 2*a + 2*b + a*b*c
+    return total_ft
